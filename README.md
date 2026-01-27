8c6312afcbb950ba1f043cba4fb9ee299f26cb08
Samuel Marchal <samuel.marchal@wakeo.co>
2026-01-27 18:16:16 +0100

    :recycle: Change flag library (licence issue)

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ You can contact me by email or use a GitHub Pull Request to send me the translat
 ## Thanks
 
 - *FluffyStuff* for providing [high-quality SVG mahjong tiles](https://github.com/FluffyStuff/riichi-mahjong-tiles) in the public domain
+- *lipis* for providing [SVG flags](https://github.com/lipis/flag-icons/tree/main) (under MIT licence)
 - *S. Bautista* for spanish translations
 - *mati* for portuguese translations
 - *\_\_\_andrew.\_\_\_* for vietnamese translations
--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ function getDownloads() {
   const data = {}
   files.forEach((file) => {
     if (file.match(/\.(pdf|jpg)$/)) {
-      const metadata = /^(?<type>.+?)-v(?<version>[\d\.]+?)-(?<language>\w{2,3})\.(?<format>.+)$/.exec(file).groups;
+      const metadata = /^(?<type>.+?)-v(?<version>[\d\.]+?)-(?<language>\w\w)\.(?<format>.+)$/.exec(file).groups;
       if (!(metadata.type in data)) {
         data[metadata.type] = {}
       }
@@ -52,7 +52,6 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: 'src/downloads', to: 'doc' },
-        { from: 'src/images', to: 'images' },
         { from: 'src/robots.txt', to: 'robots.txt' },
       ],
     }),
